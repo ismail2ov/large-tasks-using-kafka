b689e0426aa4037961e98b626848498b5c05bdf1
Ismail Ahmedov <ismail2ov@gmail.com>
2024-11-17 14:21:03 +0100

    Add project files

---
+++ src/main/java/com/github/ismail2ov/largetasks/LargeTasksUsingKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.github.ismail2ov.largetasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LargeTasksUsingKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LargeTasksUsingKafkaApplication.class, args);
+	}
+
+}
