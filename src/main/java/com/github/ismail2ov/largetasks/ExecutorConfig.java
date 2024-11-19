e7f67cee16afad7612338750ea28174741c8b9bb
Ismail Ahmedov <ismail2ov@gmail.com>
2024-11-19 08:33:46 +0100

    Config AsyncTaskExecutor to use single thread

---
+++ src/main/java/com/github/ismail2ov/largetasks/ExecutorConfig.java
@@ -0,0 +1,21 @@
+package com.github.ismail2ov.largetasks;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.AsyncTaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+public class ExecutorConfig {
+
+    @Bean
+    public AsyncTaskExecutor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(1);
+        executor.setMaxPoolSize(1);
+        executor.initialize();
+
+        return executor;
+    }
+
+}
