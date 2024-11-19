36f8e7bcdbdfe50e6b5f15debe503694d6237c79
Ismail Ahmedov <ismail2ov@gmail.com>
2024-11-19 08:01:14 +0100

    Add log

--- src/main/java/com/github/ismail2ov/largetasks/LargeTaskProcessor.java
+++ src/main/java/com/github/ismail2ov/largetasks/LargeTaskProcessor.java
@@ -1,12 +1,16 @@
 package com.github.ismail2ov.largetasks;
 
 import java.time.Duration;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class LargeTaskProcessor {
 
     public boolean run(String task) throws InterruptedException {
+        log.info("Processing task {}", task);
+
         int seconds = (task.equals("2")) ? 10 : 5;
         Thread.sleep(Duration.ofSeconds(seconds));
 
